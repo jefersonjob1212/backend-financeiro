8ff74224f846defdcf98e9afd082cf1e2347229b
Jeferson Job <33875914+jefersonjob1212@users.noreply.github.com>
2019-06-12 18:19:31 -0300

    import SpringBootServletInitializer;

--- src/main/java/br/com/db1/financiamento/FinanciamentoApplication.java
+++ src/main/java/br/com/db1/financiamento/FinanciamentoApplication.java
@@ -2,6 +2,7 @@ package br.com.db1.financiamento;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 @SpringBootApplication
 public class FinanciamentoApplication extends SpringBootServletInitializer {
