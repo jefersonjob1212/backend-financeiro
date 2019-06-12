00ed2e54538f86f4fe5f4b970afb409d240f4c53
Jeferson Job <33875914+jefersonjob1212@users.noreply.github.com>
2019-06-12 18:16:53 -0300

    First commit

---
+++ src/main/java/br/com/db1/financiamento/FinanciamentoApplication.java
@@ -0,0 +1,17 @@
+package br.com.db1.financiamento;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinanciamentoApplication extends SpringBootServletInitializer {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinanciamentoApplication.class, args);
+  }
+  
+  @Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(DemoClientApplication.class);
+	}
+}
