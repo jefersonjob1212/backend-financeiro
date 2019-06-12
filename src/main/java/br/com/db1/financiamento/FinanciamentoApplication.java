57b788fa2402a42983a6bcaecc91c51f9e2320d6
Jeferson Job <33875914+jefersonjob1212@users.noreply.github.com>
2019-06-12 18:20:56 -0300

    Remove import

--- src/main/java/br/com/db1/financiamento/FinanciamentoApplication.java
+++ src/main/java/br/com/db1/financiamento/FinanciamentoApplication.java
@@ -2,17 +2,11 @@ package br.com.db1.financiamento;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class FinanciamentoApplication extends SpringBootServletInitializer {
+public class FinanciamentoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(FinanciamentoApplication.class, args);
   }
-  
-  @Override
-	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-		return application.sources(DemoClientApplication.class);
-	}
 }
